7a2fa8a4d02440b048e19c6f54c505c7f84fd07f
Tay Jun Wen <e0261613@u.nus.edu.sg>
2020-09-06 19:49:44 +0800

    branch-Level-7 added File IO operation

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -13,6 +13,6 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + at + " " + timePeriod +")";
+        return "[E] " + super.toString() + "(at: " + at + " " + timePeriod +")";
     }
 }
