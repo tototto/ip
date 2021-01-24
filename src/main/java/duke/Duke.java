9c940b8bb7a53790957d76044b4a850f9dbd79a1
Tay Jun Wen <e0261613@u.nus.edu.sg>
2021-01-24 20:06:05 +0800

    introduced JavaFX

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -9,7 +9,23 @@ import duke.storage.ListHandler;
 
 import java.io.File;
 
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Duke extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
+    /*
     public static void main(String[] args) {
 
         // Instantiate program components
@@ -44,4 +60,5 @@ public class Duke {
             fileManager.SaveFile(list.GetList());
         }
     }
+    */
 }
